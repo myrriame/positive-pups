7051b34b8343a0a2329b1de08e92383532aaca0c
myrriame <myrriametiffany@gmail.com>
2021-03-30 21:24:39 -0400

    added javascript to make images clickable

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 alert('feeling down? look at some puppies')
+
+const panels = document.querySelectorAll('.panel')
+
+panels.forEach((panel) => {
+    panel.addEventListener('click', () => {
+        panel.classList.add('active')
+    })
+})
